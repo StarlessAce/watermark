814d9089f99292e88af642bbe7925a25d3e25434
Tomasz <udemytutorialpython@gmail.com>
2023-03-30 17:01:48 +0200

    Text watermark functions working and finished, density of watermark to add later on.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from tkinter import colorchooser
 from tkinter import ttk
 
 FONT_SIZE_LIST = (8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32)
+FONT_COLOR = 'black'
+FONT_SIZE = 16
 def enable_text():
     for child in text_editor_frame.winfo_children():
         try:
@@ -28,22 +30,36 @@ def enable_image():
         child.configure(state='disabled')
 
 
-def xxx():
-    pass
+def pick_font_size(size):
+    global FONT_SIZE
+    FONT_SIZE = size
+
 
 def pick_font_color():
-    font_color = tk.colorchooser.askcolor()[1]
-    text_editor_color_preview.configure(bg=font_color)
-
+    global FONT_COLOR
+    FONT_COLOR = tk.colorchooser.askcolor()[1]
+    text_editor_color_preview.configure(bg=FONT_COLOR)
 
 def get_text_watermark():
-    text_on_image.config(text=text_watermark.get())
+
+    pass
+    # text_on_image.config(text=text_watermark.get())
+
+
+def update_watermark_text():
+    global FONT_COLOR, FONT_SIZE
+    image_canvas1 = tk.Canvas(image_frame)
+    image_canvas1.grid(row=0, column=0, sticky='news')
+    image_canvas1.create_text(50, 50, text=text_watermark.get(), angle=rotation.get(), anchor="w", fill=FONT_COLOR, font=('Arial', FONT_SIZE))
+
+    # todolist: add density of the watermark for width and height
+    image_canvas1.create_text(100, 100, text='xxx', angle=rotation.get(), anchor="w", fill=FONT_COLOR, font=('Arial', FONT_SIZE))
+    image_canvas1.create_text(600, 600, text='xxxx', angle=rotation.get(), anchor="w", fill=FONT_COLOR, font=('Arial', FONT_SIZE))
 
 
 # Creating a window
 window = tk.Tk()
 window.title('Watermarking app')
-
 width = window.winfo_screenwidth()
 height = window.winfo_screenheight()
 
@@ -52,18 +68,25 @@ window.geometry("%dx%d" % (width, height))
 # Creating Title
 main_title = tk.Label(window, text='Add a watermark to your image', font=('Arial', 10))
 
+
+
+
+
 # Creating three frames: management, text editor, image
 management_frame = ttk.LabelFrame(window, text='Manage the file')
 editor_frame = ttk.LabelFrame(window, text='Edit your watermark')
 image_frame = ttk.LabelFrame(window, text='Your image preview')
+
+
 
 # Creating subframes in editor frame
 options_editor_frame = tk.Frame(editor_frame)
 text_editor_frame = ttk.LabelFrame(editor_frame, text='Text watermark')
 image_editor_frame = ttk.LabelFrame(editor_frame, text='Image watermark')
 
+
 # Creating subframe for text editor to make apropriate layout
-# text_editor_font_frame = tk.Frame(text_editor_frame)
+
 
 # Layout of the window
 window.grid_rowconfigure(0, weight=1)
@@ -106,7 +129,13 @@ options_editor_frame.grid_rowconfigure(1, weight=1)
 text_editor_frame.grid_rowconfigure(0, weight=1)
 text_editor_frame.grid_rowconfigure(1, weight=1)
 text_editor_frame.grid_rowconfigure(2, weight=1)
-text_editor_frame.grid_rowconfigure(3, weight=100)
+text_editor_frame.grid_rowconfigure(3, weight=1)
+text_editor_frame.grid_rowconfigure(4, weight=1)
+text_editor_frame.grid_rowconfigure(5, weight=1)
+text_editor_frame.grid_rowconfigure(6, weight=1)
+text_editor_frame.grid_rowconfigure(7, weight=1)
+text_editor_frame.grid_rowconfigure(8, weight=1)
+text_editor_frame.grid_rowconfigure(9, weight=100)
 text_editor_frame.grid_columnconfigure(0, weight=1)
 text_editor_frame.grid_columnconfigure(1, weight=6)
 
@@ -119,6 +148,7 @@ text_editor_frame.grid_columnconfigure(1, weight=6)
 # editor_frame.grid_rowconfigure(3, weight=28) #additional row to make the rest of space take 3/9
 
 image_frame.grid_columnconfigure(0, weight=1)
+image_frame.grid_rowconfigure(0, weight=1)
 ### Setting the management elements ###
 # Creating management buttons
 management_button_1 = tk.Button(management_frame, text='Upload an image')
@@ -142,26 +172,36 @@ watermark_option_2.grid(row=1, column=0, padx=15, pady=0, sticky='news')
 
 # Creating text editor elements
 text_watermark = tk.StringVar()
-chosen_font = tk.IntVar(value=8)
+chosen_font = tk.IntVar(text_editor_frame)
+chosen_font.set(FONT_SIZE)
+rotation = tk.DoubleVar(value=0)
 # text_editor_button = tk.Button(text_editor_frame, text="Text_Button_Example", relief='groove', state='disabled')
 text_editor_label = tk.Label(text_editor_frame, text="Write a text for your watermark:", state='disabled')
 text_editor_entry = tk.Entry(text_editor_frame, textvariable=text_watermark, state='disabled')
-text_editor_entry_button = tk.Button(text_editor_frame, text='OK', state='disabled', command=get_text_watermark)
+# text_editor_entry_button = tk.Button(text_editor_frame, text='OK', state='disabled', command=get_text_watermark)
 text_editor_fonts_label = tk.Label(text_editor_frame, text="Select font size:", state='disabled')
-text_editor_fonts_menu = tk.OptionMenu(text_editor_frame, chosen_font, *FONT_SIZE_LIST, command=xxx)
+text_editor_fonts_menu = tk.OptionMenu(text_editor_frame, chosen_font, *FONT_SIZE_LIST, command=pick_font_size)
 text_editor_fonts_menu.configure(state="disabled")
 text_editor_color_picker = tk.Button(text_editor_frame, text='Pick a text color', state='disabled', command=pick_font_color)
 text_editor_color_preview = tk.Button(text_editor_frame, text='            ', state='disabled', relief='groove')
+text_editor_rotation_label = tk.Label(text_editor_frame, text="Rotate the text:", state='disabled')
+text_editor_rotation_scale = tk.Scale(text_editor_frame, variable=rotation, from_=-90, to=90, orient='horizontal', state='disabled')
+text_editor_rotation_scale_button = tk.Button(text_editor_frame, text='Show watermark', state='disabled', command=update_watermark_text)
+
 
 
 
 text_editor_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky='nw')
-text_editor_entry.grid(row=1, column=0, padx=5, pady=5, sticky='new')
-text_editor_entry_button.grid(row=1, column=1, padx=7, pady=5, sticky='nw')
+text_editor_entry.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky='new')
+# text_editor_entry_button.grid(row=1, column=1, padx=7, pady=5, sticky='nw')
 text_editor_fonts_label.grid(row=2, column=0, padx=5, pady=10, sticky='nw')
 text_editor_fonts_menu.grid(row=2, column=1, padx=5, pady=5, sticky='nw')
-text_editor_color_picker.grid(row=3, column=0, padx=5, pady=5, sticky='nw')
-text_editor_color_preview.grid(row=3, column=1, padx=7, pady=5, sticky='nw')
+text_editor_color_picker.grid(row=3, column=0, padx=5, pady=10, sticky='nw')
+text_editor_color_preview.grid(row=3, column=1, padx=7, pady=10, sticky='nw')
+text_editor_rotation_label.grid(row=4, column=0, padx=2, pady=(10,5), sticky='nw')
+text_editor_rotation_scale.grid(row=5, column=0,columnspan=2, padx=2, pady=5, sticky='new')
+text_editor_rotation_scale_button.grid(row=6, column=0, columnspan=2, padx=2, pady=10, sticky='new')
+
 
 
 
@@ -190,7 +230,12 @@ image_editor_entry.grid(row=1, column=0)
 
 
 ### Setting the image elements ###
-text_on_image = tk.Label(image_frame, text='test')
-text_on_image.grid(row=0, column=0, sticky='news')
+# text_on_image = tk.Label(image_frame, text='test')
+# text_on_image.grid(row=0, column=0, sticky='news')
+
+
+print(image_frame.winfo_width())
+print(image_frame.winfo_height())
+
 
 window.mainloop()
